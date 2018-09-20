7bffe1cdc96960187d483810fdac31e938c17da2
Shryesh Khandelwal <vrocks98@gmail.com>
2018-09-21 00:29:05 +0530

    UI changes. courses.html and css

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
   $servername = "localhost";
   $username = "root";
   $password = "123456";
-  $dbname = "master";
+  $dbname = "attendance_master";
 
   // Create connection
   $conn = mysqli_connect($servername, $username, $password, $dbname);
